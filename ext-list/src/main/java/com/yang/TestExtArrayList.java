e308094c1e04efcf9cf106442aa1c8418e488972
springboot-docker-jenkins <1106991880>
2020-05-13 11:33:39 +0800

    自定义ArrayList 实现

---
+++ ext-list/src/main/java/com/yang/TestExtArrayList.java
@@ -0,0 +1,17 @@
+package com.yang;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: yang
+ * @Date: 2020-04-12 22:56
+ * @Description: List实现原理
+ */
+public class TestExtArrayList {
+
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("123");
+    }
+}
