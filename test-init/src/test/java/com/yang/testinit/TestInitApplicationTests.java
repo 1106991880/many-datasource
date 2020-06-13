3aec1de6b36e96902d38ed2ca55bd108b649e320
springboot-docker-jenkins <1106991880>
2020-06-13 19:29:04 +0800

    自定义ArrayList 实现

---
+++ test-init/src/test/java/com/yang/testinit/TestInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.testinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
